7a14cc17387acf561a3264fb0e0fa683e7d0f1c4
Nihith88 <nihith88@yandex.ru>
2022-01-25 15:19:14 +0300

    homework is done

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Создайте модуль main.py. Из модулей реализованных в заданиях 1 и 2 сделайте импорт в main.py всех функций.
+Вызовите каждую функцию в main.py и проверьте что все работает как надо.
+
+"""
+
+from Homework_4_1 import rand_el
+from Homework_3_1 import add_fold, rem_fold
+
+# add_fold()
+# rem_fold()
+
+nums = []
+print(rand_el(nums))
